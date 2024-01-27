70cff4c41fe52750eaebddfaf16272074c7a337b
malte <malteut@gmail.com>
2024-01-27 17:10:47 +0100

    Sender and Receiver created

--- crylib/crypto.cpp
+++ crylib/crypto.cpp
@@ -1,13 +1,46 @@
 #include <algorithm>
+#include <array>
+#include <bitset>
 #include <cassert>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
+#include <memory>
+#include <optional>
 #include <sodium.h>
-// #include <sodium/crypto_core_ristretto255.h>
-// #include <sodium/crypto_scalarmult_ristretto255.h>
 
 using Point = unsigned char[crypto_core_ristretto255_BYTES];
 using Scalar = unsigned char[crypto_core_ristretto255_SCALARBYTES];
+
+using std::array;
+using std::bitset;
+using std::optional;
+
+template <typename T, int N> using HeapArr = std::unique_ptr<std::array<T, N>>;
+template <int N> using HeapBits = std::unique_ptr<std::bitset<N>>;
+
+template <int N, int L> class Sender {
+  HeapArr<bitset<L>[2], N> x;
+  optional<HeapArr<Point[2], N>> h;
+
+  Sender(HeapArr<bitset<L>[2], N> x) : x(x) { h = std::nullopt; }
+};
+
+template <int N, int L> class Receiver {
+  HeapArr<Scalar, N> a;
+  HeapBits<N> o;
+
+  // should be optional
+  Point u;
+  optional<HeapArr<bitset<L>[2], N>> v;
+
+  Receiver(HeapBits<N> o) : o(o), a(std::make_unique<array<Scalar, N>>()) {
+    for (int i = 0; i < N; i++) {
+      crypto_core_ristretto255_scalar_random(a[i]);
+    }
+
+    v = std::nullopt;
+  }
+};
 
 int dh() {
   unsigned char x[crypto_core_ristretto255_HASHBYTES];
